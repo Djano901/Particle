9928bbfa8dbe0631ce9b88a7ac4214b016c70234
Jean THIERRY <jean.thierry@gadz.org>
2022-09-28 07:12:29 +0200

    Better definition of particle class

--- particle.py
+++ particle.py
@@ -1,35 +1,31 @@
 """
 Class for particle definition
 """
-from abc import ABC
 import numpy as np
 
-class Particle(ABC):
-    def __init__(self, coord):
-        self.coord = coord
+class Particle():
+    def __init__(self, coord, velocity):
+        self.coord = np.asarray(coord)
+        self.velocity = np.asarray(velocity)
+    
+    def position(self, coord):
+        self.coord = np.asarray(coord)
+
+    def speed(self, velocity):
+        self.velocity = np.asarray(velocity)
 
 class Particle2d(Particle):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def position(self):
-        return (self.x, self.y)
-
-class Particle3d(Particle2d):
-    def __init__(self, x, y, z):
-        super().__init__(x, y)
-        self.z = z
-
-    def position(self):
-        return (self.x, self.y, self.z)
+    def __init__(self, coord, velocity):
+        super().__init__(coord, velocity)
+        self.coord = np.array([coord[0], coord[1]])
+        self.velocity = np.array([velocity[0], velocity[1]])
 
 if __name__ == "__main__":
 
-    m0 = Particle(0)
-    m1 = Particle2d(0, 0)
-    m2 = Particle2d(10, 10)
-    m3 = Particle3d(0, 0, 1)
+    m0 = Particle((0, 0, 0), 0)
+    m1 = Particle2d((0, 0), (1, 0))
 
-    print(m0)
-    print(m1.position(), m2.position(), m3.position())
+    print(m0.coord)
+    print(m1.coord)
+    m1.position((1, 1))
+    print(m1.coord)
